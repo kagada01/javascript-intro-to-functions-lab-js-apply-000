17346405de1e293ddf1ed2ed082405dadcb91727
Dan Kagan <dankagan7@gmail.com>
2019-02-28 22:20:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ console.log(string.toLowerCase());
 function sayHiToGrandma(string) {
   var lowercase = "I can't hear you!"
   var uppercase = "YES INDEED!"
+  var loveYou = ""
   string.toLowerCase() === lowercase
   return lowercase
   string.toUpperCase() === uppercase
